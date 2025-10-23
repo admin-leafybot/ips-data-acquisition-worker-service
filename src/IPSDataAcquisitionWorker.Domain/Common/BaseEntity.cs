31a2725f27de9e634b8156275ff2f9d4da53c596
Sanjeev <admin@leafybot.io>
2025-10-23 07:20:55 +0530

    fix

--- src/IPSDataAcquisitionWorker.Domain/Common/BaseEntity.cs
+++ src/IPSDataAcquisitionWorker.Domain/Common/BaseEntity.cs
@@ -2,7 +2,7 @@ namespace IPSDataAcquisitionWorker.Domain.Common;
 
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public long Id { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
